d6b426da5d5239f55ac19c25c83226c3b3199e2e
Carmen Regina <crcarmen23@gmail.com>
2023-10-02 17:25:44 -0500

    added js functions for weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,76 @@
-var citySearchForm = document.querySelector
-var searchText = document.querySelector
+var citySearchForm = document.querySelector('#search-form')
+var searchText = document.querySelector('#city')
+var apiKey = 'cd405cde850f10f65e79988b0936ac58'
 
 var citySubmit = function (event) {
     event.preventDefault();
 
     var city = searchText.value.trim();
-
+console.log(city);
     if (city) {
         getWeatherData(city);
 
-        cityContainer.textContent = '';
-        
+        searchText.value = '';
+
     }
 }
 
-var getWeatherData = function () {
-    var apiUrl = 
+var getWeatherData = function (city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial'
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                return response.json()
+            }
+            else {
+                alert('Error: ' + response.statusText)
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+            getForecast(data.coord.lat, data.coord.lon)
+            displayWeather(data);
+        })
+        .catch(function (error) {
+            alert('Unable to retrieve weather data.')
+        })
+};
+
+function displayWeather(data) {
+    console.log(data)
+    document.getElementById('name').textContent= data.name
+}
+
+function getForecast (latitude, longitude) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial'
 
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
             console.log(response);
-            response.json().then(function (data) {
-                console.log(data);
-                displayWeather (data); }
-    )} else {
-        alert('Error: ' + response.statusText)
-    }
+            return response.json()
+        }
+        else {
+            alert('Error: ' + response.statusText)
+        }
+    })
+    .then(function (data) {
+        console.log(data);
+        for (var i=0; i < data.list.length; i++) {
+            if (data.list[i].dt_txt.includes('12:00:00')){
+                console.log(data.list[i])
+                var date = document.createElement('h4')
+                date.textContent= data.list[i].dt_txt
+                document.getElementById('forecast').append(date)
+                var temp = 
+            }
+        }
     })
     .catch(function (error) {
         alert('Unable to retrieve weather data.')
     })
-};
+}
 
+citySearchForm.addEventListener('submit', citySubmit)
