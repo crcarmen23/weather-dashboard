709917b6a8d3b300e4d50982f9064d286a2591e6
Carmen Regina <crcarmen23@gmail.com>
2023-10-03 17:49:20 -0500

    restructed dayjs format, added icon to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var citySubmit = function (event) {
     event.preventDefault();
 
     var city = searchText.value.trim();
-console.log(city);
+    console.log(city);
     if (city) {
         getWeatherData(city);
 
@@ -30,25 +30,28 @@ var getWeatherData = function (city) {
         })
         .then(function (data) {
             console.log(data);
-            document.getElementById('current-weather').innerHTML= ''
+            document.getElementById('current-weather').innerHTML = ''
             var card = document.createElement('div')
             card.classList.add('card')
-            var cityName = document.createElement('h3')
-            cityName.innerText= data.name
+            var cityName = document.createElement('h2')
+            cityName.innerText = data.name
             card.append(cityName)
             var date = document.createElement('h4')
-                date.textContent= dayjs.unix(data.dt).format('MM/DD/YY hh:mm')
-                card.append(date)
-            var temp =document.createElement('h4')
-                temp.textContent= 'Temp: ' + data.main.temp + '°F'
-                card.append(temp)
-                var humidity = document.createElement('h4')
-                humidity.textContent='Humidity: ' + data.main.humidity
-                card.append(humidity)
-                var wind = document.createElement('h4')
-                wind.textContent= 'Wind: ' + data.wind.speed + ' MPH'
-                card.append(wind)
-                document.getElementById('current-weather').append(card)
+            date.textContent = dayjs.unix(data.dt).format('MMM, DD hh:mm a')
+            card.append(date)
+            var icon = document.createElement('img')
+            icon.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
+            card.append(icon)
+            var temp = document.createElement('h4')
+            temp.textContent = 'Temp: ' + data.main.temp + '°F'
+            card.append(temp)
+            var humidity = document.createElement('h4')
+            humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
+            card.append(humidity)
+            var wind = document.createElement('h4')
+            wind.textContent = 'Wind: ' + data.wind.speed + ' MPH'
+            card.append(wind)
+            document.getElementById('current-weather').append(card)
             getForecast(data.coord.lat, data.coord.lon)
             displayWeather(data);
         })
@@ -62,48 +65,48 @@ function displayWeather(data) {
     console.log(data)
 }
 
-function getForecast (latitude, longitude) {
+function getForecast(latitude, longitude) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial'
 
     fetch(apiUrl)
-    .then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            return response.json()
-        }
-        else {
-            alert('Error: ' + response.statusText)
-        }
-    })
-    .then(function (data) {
-        console.log(data);
-        for (var i=0; i < data.list.length; i++) {
-            if (data.list[i].dt_txt.includes('12:00:00')){
-                console.log(data.list[i])
-                var card = document.createElement('div')
-                card.classList.add('card')
-                var date = document.createElement('h4')
-                date.textContent=data.list[i].dt_txt
-                card.append(date)
-                var icon = document.createElement('img')
-                icon.src='https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png'
-                card.append(icon)
-                var temp =document.createElement('h4')
-                temp.textContent= 'Temp: ' + data.list[i].main.temp + '°F'
-                card.append(temp)
-                var humidity = document.createElement('h4')
-                humidity.textContent='Humidity: ' + data.list[i].main.humidity
-                card.append(humidity)
-                var wind = document.createElement('h4')
-                wind.textContent= 'Wind: ' + data.list[i].wind.speed + ' MPH'
-                card.append(wind)
-                document.getElementById('forecast').append(card)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                return response.json()
             }
-        }
-    })
-    .catch(function (error) {
-        alert('Unable to retrieve weather data.')
-    })
+            else {
+                alert('Error: ' + response.statusText)
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt_txt.includes('12:00:00')) {
+                    console.log(data.list[i])
+                    var card = document.createElement('div')
+                    card.classList.add('card')
+                    var date = document.createElement('h4')
+                    date.textContent = dayjs(data.list[i].dt_txt).format('MMM DD, hh:mm a')
+                    card.append(date)
+                    var icon = document.createElement('img')
+                    icon.src = 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png'
+                    card.append(icon)
+                    var temp = document.createElement('h4')
+                    temp.textContent = 'Temp: ' + data.list[i].main.temp + '°F'
+                    card.append(temp)
+                    var humidity = document.createElement('h4')
+                    humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + '%'
+                    card.append(humidity)
+                    var wind = document.createElement('h4')
+                    wind.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH'
+                    card.append(wind)
+                    document.getElementById('forecast').append(card)
+                }
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to retrieve weather data.')
+        })
 }
 
 citySearchForm.addEventListener('submit', citySubmit)
